8f0b4257ca1472cb81526d2b3dbc743ea04a99e4
Yuan Wang <wang17@ualberta.ca>
2025-04-12 14:11:24 -0400

    Remove jest.config.js and update test and seed scripts in package.json

---
+++ jest.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', {
+      useESM: true,
+    }],
+  },
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+}; 
